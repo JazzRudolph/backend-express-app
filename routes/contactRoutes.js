ffe737fa88ea3c8fdbe08fe4dcc2a3030584a15b
Jazz Rudolph <jazzrudolph92@outlook.com>
2023-04-13 22:51:55 -0400

    Created routes for project.

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,7 +6,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
+    res.status(200).json({ message: "Get all contactsz" });
 });
 
 router.route("/").post((req, res) => {
