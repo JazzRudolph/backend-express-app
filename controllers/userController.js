cf1fcacb088f9ac6c58bf276043438f3a110ec1b
Jazz Rudolph <jazzrudolph92@outlook.com>
2023-05-04 04:14:18 -0400

    set up and verifiest jsonwebtoken for user creditionals to login.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc Register a user
@@ -42,7 +43,29 @@ const registerUser = asyncHandler(async (req, res) => {
 //@access public
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login User" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All Fields Required");
+    }
+    const user = await User.findOne({ email });
+    // Compare password with hashedpassword
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        },
+            process.env.ACCESS_TOKEN_SECERT,
+            { expiresIn: "1m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401)
+        throw new Error("Email/Password Invalid");
+    }
 });
 
 //@desc Current  user info
@@ -50,7 +73,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@access private
 
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Current User Info" });
+    res.json(req.user);
 });
 
 
